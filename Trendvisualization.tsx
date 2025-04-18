ce2bb5c1329a05cd0415ece22cda86906d1c6453
Eddy <144415950+EddyAyuketah@users.noreply.github.com>
2025-04-18 11:38:33 -0700

    Update Trendvisualization.tsx

--- Trendvisualization.tsx
+++ Trendvisualization.tsx
@@ -1,38 +1,20 @@
-Download the React DevTools for a better development experience: https://react.dev/link/react-devtools
-startup:7 Uncaught TypeError: Cannot read properties of undefined (reading 'map')
-    at TrendChart (startup:7:1)
-    at react-stack-bottom-frame (Label.js:332:1)
-    at renderWithHooks (utils.js:132:1)
-    at updateFunctionComponent (Brush.js:178:1)
-    at beginWork (CartesianAxis.js:16:1)
-    at runWithFiberInDEV (Transition.js:494:1)
-    at performUnitOfWork (ReferenceArea.js:12:1)
-    at workLoopSync (Line.js:511:1)
-    at renderRootSync (Line.js:509:1)
-    at performWorkOnRoot (Line.js:273:1)Understand this error
-Brush.js:9 An error occurred in the <TrendChart> component.
+please fix this remeber this whaole application should be gettings its data from our file in the public folder, just like the ceidtable.js code did 
+Compiled with warnings.
 
-Consider adding an error boundary to your tree to customize error handling behavior.
-Visit https://react.dev/link/error-boundaries to learn more about error boundaries.
+[eslint]
+src\components\Dashboard.js
+  Line 3:8:  '_' is defined but never used  no-unused-vars
 
-defaultOnUncaughtError @ Brush.js:9Understand this warning
+Search for the keywords to learn more about each warning.
+To ignore, add // eslint-disable-next-line to the line before.
+
+WARNING in [eslint]
+src\components\Dashboard.js
+  Line 3:8:  '_' is defined but never used  no-unused-vars
+
+webpack compiled with 1 warning
+
+
+[FETCHED DATA] CEID	AREA	ABA_LIMITER_COUNT_3DAYS	ABA_PERCENT_FLAGED_3DAYS	ABA_LIMITER_COUNT_7DAYS	ABA_PERCENT_FLAGE
 localhost/:1 Error while trying to use the following icon from the Manifest: http://localhost:3001/logo192.png (Download error or resource isn't a valid image)Understand this error
-11startup:7 [FETCHED DATA] CEID	AREA	ABA_LIMITER_COUNT_3DAYS	ABA_PERCENT_FLAGED_3DAYS	ABA_LIMITER_COUNT_7DAYS	ABA_PERCENT_FLAGE
-main.1dc3f7e3cee8e1169fd4.hot-update.js:1368 Uncaught TypeError: Cannot read properties of undefined (reading 'map')
-    at TrendAnalysisPage (main.1dc3f7e3cee8e1169fd4.hot-update.js:1368:41)
-    at react-stack-bottom-frame (Label.js:332:1)
-    at renderWithHooks (utils.js:132:1)
-    at updateFunctionComponent (Brush.js:178:1)
-    at beginWork (CartesianAxis.js:16:1)
-    at runWithFiberInDEV (Transition.js:494:1)
-    at performUnitOfWork (ReferenceArea.js:12:1)
-    at workLoopSync (Line.js:511:1)
-    at renderRootSync (Line.js:509:1)
-    at performWorkOnRoot (Line.js:273:1)Understand this error
-Brush.js:9 An error occurred in the <TrendAnalysisPage> component.
-
-Consider adding an error boundary to your tree to customize error handling behavior.
-Visit https://react.dev/link/error-boundaries to learn more about error boundaries.
-
-defaultOnUncaughtError @ Brush.js:9Understand this warning
-15startup:7 [FETCHED DATA] CEID	AREA	ABA_LIMITER_COUNT_3DAYS	ABA_PERCENT_FLAGED_3DAYS	ABA_LIMITER_COUNT_7DAYS	ABA_PERCENT_FLAGE
+2fetchCeidData.js:7 [FETCHED DATA] CEID	AREA	ABA_LIMITER_COUNT_3DAYS	ABA_PERCENT_FLAGED_3DAYS	ABA_LIMITER_COUNT_7DAYS	ABA_PERCENT_FLAGE
