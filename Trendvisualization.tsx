f1b3ebdfb418aeaa196ca3c179228f2d54c0d076
Eddy <144415950+EddyAyuketah@users.noreply.github.com>
2025-04-18 11:25:11 -0700

    Update Trendvisualization.tsx

--- Trendvisualization.tsx
+++ Trendvisualization.tsx
@@ -1,20 +1,38 @@
-ailed to compile.
+Download the React DevTools for a better development experience: https://react.dev/link/react-devtools
+startup:7 Uncaught TypeError: Cannot read properties of undefined (reading 'map')
+    at TrendChart (startup:7:1)
+    at react-stack-bottom-frame (Label.js:332:1)
+    at renderWithHooks (utils.js:132:1)
+    at updateFunctionComponent (Brush.js:178:1)
+    at beginWork (CartesianAxis.js:16:1)
+    at runWithFiberInDEV (Transition.js:494:1)
+    at performUnitOfWork (ReferenceArea.js:12:1)
+    at workLoopSync (Line.js:511:1)
+    at renderRootSync (Line.js:509:1)
+    at performWorkOnRoot (Line.js:273:1)Understand this error
+Brush.js:9 An error occurred in the <TrendChart> component.
 
-[eslint]
-src\components\TrendChart.js
-  Line 28:3:   React Hook "useEffect" is called conditionally. React Hooks must be called in the exact same order in every component render  react-hooks/rules-of-hooks
-  Line 35:21:  React Hook "useMemo" is called conditionally. React Hooks must be called in the exact same order in every component render    react-hooks/rules-of-hooks
+Consider adding an error boundary to your tree to customize error handling behavior.
+Visit https://react.dev/link/error-boundaries to learn more about error boundaries.
 
-Search for the keywords to learn more about each error.
-WARNING in [eslint]
-src\components\Dashboard.js
-  Line 3:8:  '_' is defined but never used  no-unused-vars
+defaultOnUncaughtError @ Brush.js:9Understand this warning
+localhost/:1 Error while trying to use the following icon from the Manifest: http://localhost:3001/logo192.png (Download error or resource isn't a valid image)Understand this error
+11startup:7 [FETCHED DATA] CEID	AREA	ABA_LIMITER_COUNT_3DAYS	ABA_PERCENT_FLAGED_3DAYS	ABA_LIMITER_COUNT_7DAYS	ABA_PERCENT_FLAGE
+main.1dc3f7e3cee8e1169fd4.hot-update.js:1368 Uncaught TypeError: Cannot read properties of undefined (reading 'map')
+    at TrendAnalysisPage (main.1dc3f7e3cee8e1169fd4.hot-update.js:1368:41)
+    at react-stack-bottom-frame (Label.js:332:1)
+    at renderWithHooks (utils.js:132:1)
+    at updateFunctionComponent (Brush.js:178:1)
+    at beginWork (CartesianAxis.js:16:1)
+    at runWithFiberInDEV (Transition.js:494:1)
+    at performUnitOfWork (ReferenceArea.js:12:1)
+    at workLoopSync (Line.js:511:1)
+    at renderRootSync (Line.js:509:1)
+    at performWorkOnRoot (Line.js:273:1)Understand this error
+Brush.js:9 An error occurred in the <TrendAnalysisPage> component.
 
-ERROR in [eslint]
-src\components\TrendChart.js
-  Line 28:3:   React Hook "useEffect" is called conditionally. React Hooks must be called in the exact same order in every component render  react-hooks/rules-of-hooks
-  Line 35:21:  React Hook "useMemo" is called conditionally. React Hooks must be called in the exact same order in every component render    react-hooks/rules-of-hooks
+Consider adding an error boundary to your tree to customize error handling behavior.
+Visit https://react.dev/link/error-boundaries to learn more about error boundaries.
 
-Search for the keywords to learn more about each error.
-
-webpack compiled with 1 error and 1 warning
+defaultOnUncaughtError @ Brush.js:9Understand this warning
+15startup:7 [FETCHED DATA] CEID	AREA	ABA_LIMITER_COUNT_3DAYS	ABA_PERCENT_FLAGED_3DAYS	ABA_LIMITER_COUNT_7DAYS	ABA_PERCENT_FLAGE
