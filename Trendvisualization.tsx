b6090fba3977b104abce27720ec3bb9f242c3ba2
Eddy <144415950+EddyAyuketah@users.noreply.github.com>
2025-04-22 10:56:45 -0700

    Update Trendvisualization.tsx

--- Trendvisualization.tsx
+++ Trendvisualization.tsx
@@ -1,41 +1,21 @@
-i get this error in my terminal 
+when i run: npm run start, it loads but shows up a white page and this is what my console says 
 
-Failed to compile.
-Compiled with warnings.
-
-[eslint]
-src\components\Dashboard.js
-  Line 3:8:  '_' is defined but never used  no-unused-vars
-
-src\components\TrendChart.js
-  Line 55:6:  React Hook useMemo has a missing dependency: 'selectedArea'. Either include it or remove the dependency array  react-hooks/exhaustive-deps
-
-Search for the keywords to learn more about each warning.
-To ignore, add // eslint-disable-next-line to the line before.
-
-WARNING in [eslint]
-src\components\Dashboard.js
-  Line 3:8:  '_' is defined but never used  no-unused-vars
-
-src\components\TrendChart.js
-  Line 55:6:  React Hook useMemo has a missing dependency: 'selectedArea'. Either include it or remove the dependency array  react-hooks/exhaustive-deps
-
-webpack compiled with 1 warning
-
-and i get this error in my react app
-
-Uncaught runtime errors:
-×
-ERROR
-selectedCeids.forEach is not a function
-TypeError: selectedCeids.forEach is not a function
-    at http://localhost:3001/main.2a8ac4f229c372bb2e0a.hot-update.js:31:19
+Download the React DevTools for a better development experience: https://react.dev/link/react-devtools
+dataTransformations.js:10 Uncaught TypeError: selectedCeids.forEach is not a function
+    at dataTransformations.js:10:1
     at Array.map (<anonymous>)
-    at getTrendData (http://localhost:3001/main.2a8ac4f229c372bb2e0a.hot-update.js:27:18)
-    at Dashboard (http://localhost:3001/static/js/bundle.js:82331:87)
-    at react-stack-bottom-frame (http://localhost:3001/static/js/bundle.js:53345:18)
-    at renderWithHooks (http://localhost:3001/static/js/bundle.js:43555:20)
-    at updateFunctionComponent (http://localhost:3001/static/js/bundle.js:45248:17)
-    at beginWork (http://localhost:3001/static/js/bundle.js:45834:16)
-    at runWithFiberInDEV (http://localhost:3001/static/js/bundle.js:41327:68)
-    at performUnitOfWork (http://localhost:3001/static/js/bundle.js:47907:93)
+    at getTrendData (dataTransformations.js:7:1)
+    at Dashboard (Dashboard.js:357:1)
+    at react-stack-bottom-frame (react-dom-client.development.js:23863:1)
+    at renderWithHooks (react-dom-client.development.js:5529:1)
+    at updateFunctionComponent (react-dom-client.development.js:8897:1)
+    at beginWork (react-dom-client.development.js:10522:1)
+    at runWithFiberInDEV (react-dom-client.development.js:1518:1)
+    at performUnitOfWork (react-dom-client.development.js:15130:1)Understand this error
+react-dom-client.development.js:8283 An error occurred in the <Dashboard> component.
+
+Consider adding an error boundary to your tree to customize error handling behavior.
+Visit https://react.dev/link/error-boundaries to learn more about error boundaries.
+
+defaultOnUncaughtError @ react-dom-client.development.js:8283Understand this warning
+localhost/:1 Error while trying to use the following icon from the Manifest: http://localhost:3001/logo192.png (Download error or resource isn't a valid image)
